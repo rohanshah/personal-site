0194fcc73161a5a96f0a19d18dba89049ad5a81a
Rohan Shah <rmshah91@gmail.com>
2014-01-05 03:38:46 -0500

    Add venue filter

--- js/controllers/musicCtrl.js
+++ js/controllers/musicCtrl.js
@@ -3,6 +3,13 @@ var musicApp = angular.module('musicApp', []);
 musicApp.controller('MusicCtrl', function ($scope, $http) {
 
 	$scope.events = {};
+	$scope.venues = ["Union Transfer", "Milkboy"];
+
+	$scope.venueFilter ={};
+	for (venue in $scope.venues) {
+		$scope.venueFilter[$scope.venues[venue]] = true;
+	}
+
 	$http.get('philly-concerts/events.json').success(function (result) {
 		$scope.events = result;
 	});
