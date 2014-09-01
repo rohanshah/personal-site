22f0622b689f9cb2bdd2b0260685cd55a398615f
Rohan Shah <rmshah91@gmail.com>
2014-09-01 14:27:29 -0400

    get website mostly set up

--- index.py
+++ index.py
@@ -2,9 +2,17 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello Again Again, World!')
+        html = open("index.html", 'r').read()
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(html)
+
+class MusicPage(webapp2.RequestHandler):
+    def get(self):
+        html = open("music.html", 'r').read()
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(html)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/music', MusicPage)
 ], debug=True)
